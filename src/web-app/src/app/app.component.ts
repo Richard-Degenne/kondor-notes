71a509b9b0ad27abba203bf3c3053a22dece95cd
Richard Degenne <richard.degenne@perfect-memory.com>
2020-11-09 09:52:38 +0100

    Added Material navigation

--- src/web-app/src/app/app.component.ts
+++ src/web-app/src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'Kondor Notes';
-}
+export class AppComponent { }
---
+++ src/web-app/src/app/menu/menu.component.spec.ts
@@ -0,0 +1,52 @@
+import { LayoutModule } from '@angular/cdk/layout';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { MenuComponent } from './menu.component';
+
+describe('MenuComponent', () => {
+  let component: MenuComponent;
+  let fixture: ComponentFixture<MenuComponent>;
+  let compiled;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [MenuComponent],
+      imports: [
+        NoopAnimationsModule,
+        LayoutModule,
+        MatButtonModule,
+        MatIconModule,
+        MatListModule,
+        MatSidenavModule,
+        MatToolbarModule,
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MenuComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    compiled = fixture.nativeElement;
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have a Home link', () => {
+    expect(compiled.querySelector('.sidenav a[routerLink="/"]').textContent)
+      .toContain('Home');
+  });
+
+  it('should render the title', () => {
+    expect(compiled.querySelector('.sidenav-content span').textContent)
+      .toContain('Kondor Notes')
+  });
+});
