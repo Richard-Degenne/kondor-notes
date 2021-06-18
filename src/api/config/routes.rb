fe007c4d57acbfaa7566b861254a058e52c4678f
Richard Degenne <richdeg2@gmail.com>
2021-06-18 17:16:17 +0200

    Added ping

--- src/api/config/routes.rb
+++ src/api/config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'ping' => proc { |_env|
+    ['200', {}, 'pong']
+  }
 end
